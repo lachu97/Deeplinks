82b01b8cca41be1f173716d34f81728b4ee7e1c9
lakshu <ced15i004@iiitdm.ac.in>
2022-03-28 15:24:36 +0530

    Deeplinks Issue SOLVED

--- app/src/main/java/com/example/deeplinks/AnotherActivity.kt
+++ app/src/main/java/com/example/deeplinks/AnotherActivity.kt
@@ -4,6 +4,12 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 class AnotherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +17,23 @@ class AnotherActivity : AppCompatActivity() {
 
         val action: String? = intent?.action
         val data: Uri? = intent?.data
-        Log.i("MainActivity","Action = ${action}")
-        Log.i("MainActivity","Data = ${data}")
+        if(action!=null){
+            data.let {
+                Log.i("MainActivity","Action = ${action}")
+                Log.i("MainActivity","Data = ${data}")
+            }
+        }
+        setContent {
+            Scaffold(
+            modifier = Modifier
+                    .fillMaxSize()) {
+                Greet(data!!.path.toString() )
+            }
+        }
     }
+}
+
+@Composable
+fun Greet(text:String) {
+    Text(text = "${text}")
 }
